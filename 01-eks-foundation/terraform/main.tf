f8226071b5d817ed45c07a4a6fde7d4442d2e2b9
Midhun Balachandran <midhun77msdcr@gmail.com>
2026-06-17 11:08:15 +0530

    feat: create VPC networking foundation for EKS

--- 01-eks-foundation/terraform/main.tf
+++ 01-eks-foundation/terraform/main.tf
@@ -51,3 +51,34 @@ resource "aws_subnet" "private_2" {
     Name = "private-subnet-2"
   }
 }
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.eks_vpc.id
+
+  tags = {
+    Name = "eks-foundation-igw"
+  }
+}
+
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.eks_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "public-route-table"
+  }
+}
+
+resource "aws_route_table_association" "public_1_assoc" {
+  subnet_id      = aws_subnet.public_1.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+resource "aws_route_table_association" "public_2_assoc" {
+  subnet_id      = aws_subnet.public_2.id
+  route_table_id = aws_route_table.public_rt.id
+}
